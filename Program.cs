5da68168afea9020af3d2883650a5e9f0dcc9574
Johnny-27 <johnycal189@gmail.com>
2022-09-08 16:58:50 -0600

    Order of Operations Method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
 ﻿//WorkWithIntegers();
 
-void WorkWithIntegers()
+void BasicEquations()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+void OrderOfOperations()
 {
     int a = 5;
     int b = 4;
     int c = 2;
-    int d = (a + b) * c;
+    int d = a + b * c;
     Console.WriteLine(d);
 }
