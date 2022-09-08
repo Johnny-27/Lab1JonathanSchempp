9c98c76b564eb55a8a1b4390d192013452987cb5
Johnny-27 <johnycal189@gmail.com>
2022-09-08 17:26:25 -0600

    overflow example

--- Program.cs
+++ Program.cs
@@ -34,4 +34,6 @@ void OrderPrecedence()
     int max = int.MaxValue;
     int min = int.MinValue;
     Console.WriteLine($"The range of the integers is {min} to {max}");
+    int what = max + 3;
+    Console.WriteLine($"An example of overflow: {what}");
 }
