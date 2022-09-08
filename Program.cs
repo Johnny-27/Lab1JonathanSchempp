86dbdf7df473fb9be2ff1b35e002849c61e47ea0
Johnny-27 <johnycal189@gmail.com>
2022-09-08 17:00:55 -0600

    Interesting Integer Behavior

--- Program.cs
+++ Program.cs
@@ -22,9 +22,9 @@ void BasicEquations()
 }
 void OrderOfOperations()
 {
-    int a = 5;
-    int b = 4;
-    int c = 2;
-    int d = a + b * c;
-    Console.WriteLine(d);
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
 }
