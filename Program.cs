1424bda568d4bfb9589014cef927a95a00453f7d
Johnny-27 <johnycal189@gmail.com>
2022-09-08 17:36:03 -0600

    scientific notation

--- Program.cs
+++ Program.cs
@@ -62,4 +62,8 @@ void DoubleType()
     double g = 8;
     double h = (e + f) / g;
     Console.WriteLine(h);
+//scientific notation;
+    double max = double.MaxValue;
+    double min = double.MinValue;
+    Console.WriteLine($"The range of double is {min} to {max}");
 }
