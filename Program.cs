82d3a8f00ed6c6c14ecd3a9ebe13c0eb697876bc
Johnny-27 <johnycal189@gmail.com>
2022-09-08 17:13:37 -0600

    Order Precedence

--- Program.cs
+++ Program.cs
@@ -24,6 +24,18 @@ void WorkWithIntegers()
 
 void OrderPrecedence()
 {
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
     int e = 7;
     int f = 4;
     int g = 3;
